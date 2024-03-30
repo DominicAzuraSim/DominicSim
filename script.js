059f931df060ee4536b7d6d0c945e0c82e11730a
Dominic Sim <165442221+DominicAzuraSim@users.noreply.github.com>
2024-03-30 18:08:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const pages = ['Resume', 'CV', 'Media', 'About'];
+    const pages = ['Resume', 'CV', 'Media', 'Hobbies'];
     const sidebar = document.getElementById('sidebar');
 
     pages.forEach(page => {
